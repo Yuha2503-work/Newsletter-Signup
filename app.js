83a924b193a1c0c6ff272f1e28af72bbf52806b0
yuha2503 <yuh.a.work.2503@gmail.com>
2020-12-14 11:13:18 +0700

    Done request to Mailchimp

--- app.js
+++ app.js
@@ -10,15 +10,52 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/signup.html")
+  res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/", function(req, res){
-  console.log(req.body.firstName);
-  console.log(req.body.lastName);
-  console.log(req.body.email);
-})
+
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address : email,
+        status : "subscribed",
+        merge_fields : {
+          FNAME : firstName,
+          LNAME : lastName,
+        }
+      }
+    ]
+  }
+
+  var jsondata = JSON.stringify(data);
+
+  var option = {
+    url : "https://us7.api.mailchimp.com/3.0/lists/9e4a2390ec",
+    method : "POST",
+    headers : { "authorization" : "yuhA2503 683ba16ba35116f16e6568a4d4a008c0-us7" },
+    body : jsondata,
+  };
+
+  request(option, function(error, response, body){
+    if (error) {
+      console.log(error);
+    }
+    else {
+      console.log(response.statusCode);
+    }
+  });
+
+});
 
 app.listen(3000, function(){
   console.log("Run success");
 });
+
+
+//apiKey:9e4a2390ec
+//683ba16ba35116f16e6568a4d4a008c0-us7
